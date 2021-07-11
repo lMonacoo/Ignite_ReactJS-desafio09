3f2bfdf640e9b6140d31ff847766a613982a439d
lMonacoo <lucas.monaco.maquina@gmail.com>
2021-07-11 13:43:43 -0300

    :bug:

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -44,29 +44,36 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (relevantEvents.has(type)) {
       try {
-        switch (type) {
-          case 'customer.subscription.updated':
-          case 'customer.subscription.deleted':
-            const subscription = event.data.object as Stripe.Subscription;
+        console.log('enter in function');
+        const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
-            await saveSubscription(subscription.id, subscription.customer.toString(), false);
-            break;
+        await saveSubscription(
+          checkoutSession.subscription.toString(),
+          checkoutSession.customer.toString(),
+          true
+        );
+        // switch (type) {
+        //   case 'customer.subscription.updated':
+        //   case 'customer.subscription.deleted':
+        //     const subscription = event.data.object as Stripe.Subscription;
 
-          case 'checkout.session.completed':
-            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+        //     await saveSubscription(subscription.id, subscription.customer.toString(), false);
+        //     break;
 
-            console.log(event.data.object);
+        //   case 'checkout.session.completed':
+        //     const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
-            await saveSubscription(
-              checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString(),
-              true
-            );
+        //     console.log(event.data.object);
 
-            break;
-          default:
-            throw new Error('Unhandled event.');
-        }
+        //     await saveSubscription(
+        //       checkoutSession.subscription.toString(),
+        //       checkoutSession.customer.toString(),
+        //       true
+        //     );
+
+        //     break;
+        //   default:
+        //     throw new Error('Unhandled event.');
       } catch (err) {
         // sentry, bugsnag <- para avisar que tem erro
         return res.json({ error: 'Webhook handler failed.' });
